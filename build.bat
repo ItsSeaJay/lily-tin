f963b95357ae697a19d8768a4d33320781799f82
Unknown <b6010572@my.shu.ac.uk>
2018-07-03 18:59:22 +0000

    Added helper batch scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+REM A helper script for building Lily Tin on Windows
+REM NOTE: You must have generated the project in CMake first
+REM NOTE: This might require the specific Visual Studio command prompt to work
 @echo off
 
 msbuild bin/LilyTin.sln
--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-:: A helper script for running the debug version of Lily Tin
-:: NOTE: This will only work once Lily Tin has been generated and built
+REM A helper script for running the debug version of Lily Tin
+REM NOTE: This will only work once Lily Tin has been generated and built
 @echo off
 
 bin\Debug\LilyTin.exe
